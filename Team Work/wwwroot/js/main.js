5df758d19d6bf2ce1005544845a453debf6ec085
kamandar0047 <82367116+kamandar0047@users.noreply.github.com>
2021-10-27 04:21:50 +1100

    team

---
+++ Team Work/wwwroot/js/main.js
@@ -0,0 +1,145 @@
+(function ($) {
+    "use strict";
+    let skip = 12;
+    let take = 8;
+    let theacher_salary = $("salary").val();
+    $(document).on("click", "#loadmore", function () {
+        $.ajax({
+            url: '/Theacher/LoadMore?skip={}&take=8',
+            type: "Get",
+            success: function (res) { $(."theacher").append(res);
+        skip += take;
+        if (skip >= theacher_salary) {
+            $("#loadmore").remove();
+        }
+    }
+        
+        })
+    })
+/*------------------------------------
+	Sticky Menu 
+--------------------------------------*/
+ var windows = $(window);
+    var stick = $(".header-sticky");
+	windows.on('scroll',function() {    
+		var scroll = windows.scrollTop();
+		if (scroll < 5) {
+			stick.removeClass("sticky");
+		}else{
+			stick.addClass("sticky");
+		}
+	});  
+/*------------------------------------
+	jQuery MeanMenu 
+--------------------------------------*/
+	$('.main-menu nav').meanmenu({
+		meanScreenWidth: "767",
+		meanMenuContainer: '.mobile-menu'
+	});
+    
+    
+    /* last  2 li child add class */
+    $('ul.menu>li').slice(-2).addClass('last-elements');
+/*------------------------------------
+	Owl Carousel
+--------------------------------------*/
+    $('.slider-owl').owlCarousel({
+        loop:true,
+        nav:true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 2500,
+        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    $('.partner-owl').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });  
+
+    $('.testimonial-owl').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+/*------------------------------------
+	Video Player
+--------------------------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        zoom: {
+            enabled: true,
+        }
+    });
+    
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+    }); 
+/*----------------------------
+    Wow js active
+------------------------------ */
+    new WOW().init();
+/*------------------------------------
+	Scrollup
+--------------------------------------*/
+    $.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    });
+/*------------------------------------
+	Nicescroll
+--------------------------------------*/
+     $('body').scrollspy({ 
+            target: '.navbar-collapse',
+            offset: 95
+        });
+$(".notice-left").niceScroll({
+            cursorcolor: "#EC1C23",
+            cursorborder: "0px solid #fff",
+            autohidemode: false,
+            
+        });
+
+})(jQuery);	
